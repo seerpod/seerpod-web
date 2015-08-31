968926b371e9a1d1cf1ff15e0dc536a5dceb9271
Tarun Kumar <tarun@nextdoor.com>
2015-08-30 20:11:59 -0700

    Updating readme

--- seerpod.py
+++ seerpod.py
@@ -16,10 +16,10 @@ import count_encoder
 from tornado.options import define, options
 
 define("port", default=8888, help="run on the given port", type=int)
-define("mysql_host", default="dev.cisege0e6wes.us-west-2.rds.amazonaws.com:3306", help="blog database host")
-define("mysql_database", default="seerpod", help="blog database name")
-define("mysql_user", default="root", help="blog database user")
-define("mysql_password", default="5eerp0d1nc", help="blog database password")
+define("mysql_host", default="dev.cisege0e6wes.us-west-2.rds.amazonaws.com:3306", help="seerpod database host")
+define("mysql_database", default="seerpod", help="seerpod database name")
+define("mysql_user", default="root", help="seerpod database user")
+define("mysql_password", default="5eerp0d1nc", help="seerpod database password")
 
 
 class Application(tornado.web.Application):
